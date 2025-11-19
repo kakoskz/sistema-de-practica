0a4ff4e264d96e2412e889569c509aa667c1cd60
kakoskz <francoescares5@gmail.com>
2025-11-19 13:51:30 -0300

    Creo tabla Centro Practica
    ID posee el atributo de identity para que se genere automaticamente el id sin que uno deba manejar eso

--- base de datps/Tablas.sql
+++ base de datps/Tablas.sql
@@ -1,10 +1,19 @@
 CREATE TABLE Estudiante(
-    nombreCompleto VARCHAR(200),
-    rut INT PRIMARY KEY,
+    nombreCompleto VARCHAR(200) NOT NULL,
+    rut INT PRIMARY KEY ,
     anoLectivo VARCHAR(10),
     domicilio VARCHAR(50),
     telefono VARCHAR(12),
-    correoInstitucional VARCHAR(100),
-    carrera VARCHAR(50)
+    correoInstitucional VARCHAR(100) NOT NULL,
+    carrera VARCHAR(50) NOT NULL
 );
 
+
+CREATE TABLE CentroPractica(
+    idCentroPractica INT IDENTITY(1,1) PRIMARY KEY,
+    rutEmpresa VARCHAR(12) NOT NULL,
+    nombre VARCHAR(150) NOT NULL,
+    descripcion VARCHAR(500),
+    habilidadesEsperadas TEXT ,
+    direccion VARCHAR(250) NOT NULL
+)
