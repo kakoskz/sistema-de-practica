995b74bc7097aac59806aa85d04152ad3892d7aa
kakoskz <francoescares5@gmail.com>
2025-11-19 13:23:16 -0300

    Creo tabla Estudiante
    Agregando primera tabla de la base de datos

---
+++ base de datps/sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Estudiante(
+    nombreCompleto VARCHAR(200),
+    rut INT PRIMARY KEY,
+    añoLectivo VARCHAR(10),
+    domicilio VARCHAR(50),
+    telefono VARCHAR(12),
+    correoInstitucional VARCHAR(100),
+    carrera VARCHAR(50)
+);
